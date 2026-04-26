c97ca1a2db403c250b5641822e7f499b6a5479ab
xl2024 <xl5333@nyu.edu>
2026-04-26 07:46:50 -0400

    update

--- src/plots/rsa_1c.py
+++ src/plots/rsa_1c.py
@@ -176,11 +176,11 @@ def main():
 
         shapes = [o1['shape'], o2['shape'], o3['shape'], o4['shape']]
         colors = [o1['color'], o2['color'], o3['color'], o4['color']]
-        coords = [(0,0), (0,1), (1,0), (1,1)]
+        coords = [(0,0), (0,23), (23,0), (23,23)]
 
         img = generate_custom_image(
-            cols=2, 
-            rows=2, 
+            cols=24, 
+            rows=24, 
             shapes=shapes,
             colors=colors,
             coords=coords

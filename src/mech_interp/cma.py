517f7f3682d8be257e76ebc32e5bd182db9978bc
xl2024 <xl5333@nyu.edu>
2026-05-14 13:46:27 -0400

    update

--- src/mech_interp/cma.py
+++ src/mech_interp/cma.py
@@ -49,9 +49,10 @@ def cma_headwise(
         with model.trace() as tracer:
             with tracer.invoke(**inputs_c1):
                 # Safely slice 3D logit tensor preserving batch dim
-                clean_logits = model.lm_head.output[:, token_pos[0]:token_pos[1], :]
+                clean_logits = model.lm_head
 
     # Calculate Baseline Clean Term: M(c1)[a1*] - M(c1)[a1]
+    print("clean_logits", clean_logits)
     base_a1_logit = clean_logits[0, :, a1_id].mean().item()
     base_a1_star_logit = clean_logits[0, :, a1_star_id].mean().item()
     base_term = base_a1_star_logit - base_a1_logit

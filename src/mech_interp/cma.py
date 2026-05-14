333025a0af5609c5b63032af789b15b682fc2042
xl2024 <xl5333@nyu.edu>
2026-05-14 13:12:38 -0400

    rm save.cpu

--- src/mech_interp/cma.py
+++ src/mech_interp/cma.py
@@ -40,7 +40,7 @@ def cma_headwise(
                     layer_module = _resolve_layer_path(model, layer_template.format(l))
                     # Safely intercept full 3D tensor: [batch, seq_len, hidden_dim]
                     attn_out = layer_module.self_attn.o_proj.input[0]
-                    c2_head_cache[l] = einops.rearrange(attn_out, 's (h d) -> s h d', h=num_heads).save().cpu()
+                    c2_head_cache[l] = einops.rearrange(attn_out, 's (h d) -> s h d', h=num_heads)
         
         gc_collect()
 
@@ -49,7 +49,7 @@ def cma_headwise(
         with model.trace() as tracer:
             with tracer.invoke(**inputs_c1):
                 # Safely slice 3D logit tensor preserving batch dim
-                clean_logits = model.lm_head.output[:, token_pos[0]:token_pos[1], :].save().cpu()
+                clean_logits = model.lm_head.output[:, token_pos[0]:token_pos[1], :]
 
         gc_collect()
 
@@ -81,7 +81,7 @@ def cma_headwise(
                         hs_input[:] = einops.rearrange(hs_heads, 's h d -> s (h d)')
                         
                         # Capture patched output logits safely
-                        patched_logits = model.lm_head.output[:, token_pos[0]:token_pos[1], :].save().cpu()
+                        patched_logits = model.lm_head.output[:, token_pos[0]:token_pos[1], :]
 
                 gc_collect()
             

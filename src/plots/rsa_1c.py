790be5c25cf5f3ddf9b35416b9bb15c70dfb16c9
xl2024 <xl5333@nyu.edu>
2026-05-18 09:12:41 -0400

    update

--- src/plots/rsa_1c.py
+++ src/plots/rsa_1c.py
@@ -18,8 +18,8 @@ from src.utils.tools import predict, get_num_hidden_layers, load_config, get_per
 # model_id = "llava-hf/llava-1.5-13b-hf"
 # model_id = "bczhou/tiny-llava-v1-hf"
 # model_id = "llava-hf/llava-onevision-qwen2-7b-ov-hf"        # Figure 18
-model_id = "HuggingFaceM4/idefics2-8b-chatty"
-# model_id = "HuggingFaceM4/idefics2-8b"
+# model_id = "HuggingFaceM4/idefics2-8b-chatty"
+model_id = "HuggingFaceM4/idefics2-8b"
 
 def plot_rsa_figure_1c(
     rsa_scores_prompt: Dict[str, List[float]],

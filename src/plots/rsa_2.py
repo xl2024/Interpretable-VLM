3968e21bf1902ef4bbd7d49c87a5d2661bd3c81e
xl2024 <xl5333@nyu.edu>
2026-05-18 10:31:36 -0400

    update

--- src/plots/rsa_2.py
+++ src/plots/rsa_2.py
@@ -10,8 +10,8 @@ from src.data.synthetic_generator import generate_custom_image
 from src.utils.tools import predict, get_num_hidden_layers, load_config, get_text_prompt, get_permutations
 
 
-model_id = "Qwen/Qwen2-VL-7B-Instruct"
-# model_id = "Qwen/Qwen2.5-VL-3B-Instruct"                    # Figure 
+# model_id = "Qwen/Qwen2-VL-7B-Instruct"
+model_id = "Qwen/Qwen2.5-VL-3B-Instruct"                    # Figure 
 # model_id = "Qwen/Qwen2.5-VL-7B-Instruct"                    # Figure 
 # model_id = "Qwen/Qwen2.5-VL-32B-Instruct"                   # Figure 
 # model_id = "llava-hf/llava-1.5-7b-hf"                       # Figure 

c04c88dc937749a1993ee2d5acfae172bca660b6
xl2024 <xl5333@nyu.edu>
2026-05-18 10:35:44 -0400

    update

--- src/plots/rsa_2.py
+++ src/plots/rsa_2.py
@@ -12,12 +12,12 @@ from src.utils.tools import predict, get_num_hidden_layers, load_config, get_tex
 
 # model_id = "Qwen/Qwen2-VL-7B-Instruct"
 # model_id = "Qwen/Qwen2.5-VL-3B-Instruct"                    # Figure 
-model_id = "Qwen/Qwen2.5-VL-7B-Instruct"                    # Figure 
+# model_id = "Qwen/Qwen2.5-VL-7B-Instruct"                    # Figure 
 # model_id = "Qwen/Qwen2.5-VL-32B-Instruct"                   # Figure 
 # model_id = "llava-hf/llava-1.5-7b-hf"                       # Figure 
 # model_id = "llava-hf/llava-1.5-13b-hf"
 # model_id = "bczhou/tiny-llava-v1-hf"
-# model_id = "llava-hf/llava-onevision-qwen2-7b-ov-hf"        # Figure 18
+model_id = "llava-hf/llava-onevision-qwen2-7b-ov-hf"        # Figure
 # model_id = "HuggingFaceM4/idefics2-8b-chatty"
 # model_id = "HuggingFaceM4/idefics2-8b"
 

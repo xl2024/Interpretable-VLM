9f2503bf18385834548283ef7abafe78d2d9559a
xl2024 <xl5333@nyu.edu>
2026-05-22 22:52:59 -0400

    update

--- src/plots/cma_29.py
+++ src/plots/cma_29.py
@@ -146,7 +146,7 @@ def main():
     #              "llava-hf/llava-1.5-7b-hf",
     #              "Qwen/Qwen2.5-VL-3B-Instruct",
     #              ]
-    model_ids = ["Qwen/Qwen2.5-VL-32B-Instruct"]
+    model_ids = ["llava-hf/llava-1.5-13b-hf"]
 
     filename = "src/data/cma/figure_29_results.npz"
     file_path = Path(filename)

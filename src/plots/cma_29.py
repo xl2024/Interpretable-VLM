c2fb29d9ad6cbf21620d55d7b1537d5e6956f8f0
xl2024 <xl5333@nyu.edu>
2026-05-22 22:25:32 -0400

    update

--- src/plots/cma_29.py
+++ src/plots/cma_29.py
@@ -153,6 +153,7 @@ def main():
     if file_path.exists():
         print(f"Found {filename}! Loading cma results for figure 29...")
         fig_29_results = np.load(filename)
+        fig_29_results = dict(fig_29_results)
     else:
         fig_29_results = {}
 

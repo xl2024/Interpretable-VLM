8a737fbc80f8f4bbb9456782353b797effd4eb60
xl2024 <xl5333@nyu.edu>
2026-05-26 14:36:09 -0400

    update

--- src/plots/cma_3.py
+++ src/plots/cma_3.py
@@ -148,7 +148,6 @@ def get_patching_results(model, processor, num_layers, num_heads, top_k_heads, l
                 alpha=alpha,
                 d_o_head_cache=left_d_o
             )
-            print("left_patching_results:", left_patching_results)
             left_patching_results[str(alpha)].append([image_data["right_color"], predicted_word])
         
             right_prompt = f"In this image there is a {image_data['left_color']} {image_data['left_animal']} and a"
@@ -177,8 +176,8 @@ def get_patching_results(model, processor, num_layers, num_heads, top_k_heads, l
             )
             right_patching_results[str(alpha)].append([image_data["left_color"], predicted_word])
 
-        print_results(alpha, "left", left_patching_results[alpha])
-        print_results(alpha, "right", right_patching_results[alpha])
+        print_results(alpha, "left", left_patching_results[str(alpha)])
+        print_results(alpha, "right", right_patching_results[str(alpha)])
 
     return left_patching_results, right_patching_results
 

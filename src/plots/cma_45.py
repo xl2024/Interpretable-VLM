24e297bce262ff2a5c2b0703f97209eabd7e84b6
xl2024 <xl5333@nyu.edu>
2026-05-27 23:40:40 -0400

    update

--- src/plots/cma_45.py
+++ src/plots/cma_45.py
@@ -131,8 +131,8 @@ def run_cma_coco_unit(
         img = target_images[i]
         o_0 = object_mapping[target_filenames[i]]['O_0']
         
-        intervention_prompt = f"In this image there is 1. a {o_0} 2. a"
-        intervention_prompt_text = get_text_prompt(model, intervention_prompt, img, processor, "starting by an object")
+        intervention_prompt = f"In this image there is 1. a {o_0} 2. a "
+        intervention_prompt_text = get_text_prompt(model, intervention_prompt, img, processor, "[OBJECT]")
         
         predicted_word = cma_head_patching_by_logits(
             model=model,

d78ce22f186bd4ba7bd9dd5622163dec63a9a2c4
xl2024 <xl5333@nyu.edu>
2026-05-27 21:52:59 -0400

    update

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -32,14 +32,12 @@ def predict(
         
         gc_collect()
         
-    if True:
+    if new_only:
         input_length = inputs["input_ids"].shape[1]
         new_tokens = output[0][input_length:]
-        generated_text1 = processor.tokenizer.decode(new_tokens, skip_special_tokens=True)
-    # else:
+        generated_text = processor.tokenizer.decode(new_tokens, skip_special_tokens=True)
+    else:
         generated_text = processor.decode(output[0], skip_special_tokens=True)
-        print("generated_text:", generated_text)
-        print("generated_text_new:", generated_text1)
     # print(f"Model predicted: '{generated_text.strip()}'")
     
     return generated_text

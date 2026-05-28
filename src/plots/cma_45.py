f169ecfb318007113caa59b53cb3119867e9da99
xl2024 <xl5333@nyu.edu>
2026-05-28 15:26:27 -0400

    update

--- src/plots/cma_45.py
+++ src/plots/cma_45.py
@@ -44,7 +44,7 @@ def get_coco_objects(model, processor, coco_val_dir, cache_file, max_images=100)
     
     for idx, img_path in enumerate(all_image_paths):
         filename = os.path.basename(img_path)
-        print("filename", filename)
+        print("filename:", filename)
         
         try:
             img = Image.open(img_path).convert('RGB')

261b64e119697890ab23031f2a55a93d5a65c61d
xl2024 <xl5333@nyu.edu>
2026-05-28 15:14:41 -0400

    update

--- src/plots/cma_45.py
+++ src/plots/cma_45.py
@@ -40,7 +40,7 @@ def get_coco_objects(model, processor, coco_val_dir, cache_file, max_images=100)
         
     object_mapping = {}
     prompt = "In this image there is 1. a"
-    system_format = "OBJECT1 2. a OBJECT2, replacing OBJECT1 and OBJECT2 with the first and second object in the image, respectively. Do not repeat the prompt words, just complete it"
+    system_format = "OBJECT1 2. a OBJECT2, replacing OBJECT1 and OBJECT2 with the first and second object in the image, respectively. Do not repeat the prompt words, just append with the requested format"
     
     for idx, img_path in enumerate(all_image_paths):
         filename = os.path.basename(img_path)
@@ -113,7 +113,7 @@ def run_cma_coco_unit(
     print("Estimating average position ID (IDO_0) from the Source Set...")
     
     source_prompt = "In this image there is 1. a"
-    system_format = "OBJECT1 2. a OBJECT2, replacing OBJECT1 and OBJECT2 with the first and second object in the image, respectively. Do not repeat the prompt words, just complete it"
+    system_format = "OBJECT1 2. a OBJECT2, replacing OBJECT1 and OBJECT2 with the first and second object in the image, respectively. Do not repeat the prompt words, just append with the requested format"
     source_prompt_texts = [
         get_text_prompt(model, source_prompt, img, processor, system_format) for img in source_images
     ]
@@ -136,7 +136,7 @@ def run_cma_coco_unit(
         o_1 = object_mapping[target_filenames[i]]['O_1']
         
         intervention_prompt = f"In this image there is 1. a {o_0} 2. a"
-        intervention_system_format = "OBJECT, replacing OBJECT with the second object in the image. Do not repeat the prompt words, just complete it"
+        intervention_system_format = "OBJECT, replacing OBJECT with the second object in the image. Do not repeat the prompt words, just append with the requested format"
         intervention_prompt_text = get_text_prompt(model, intervention_prompt, img, processor, intervention_system_format)
         token_pos = get_token_position(processor, intervention_prompt_text, img, intervention_prompt_text[-1], False)
         

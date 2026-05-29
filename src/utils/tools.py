7bb6dec1b96552d9138a60cc37daaf5a489522c8
xl2024 <xl5333@nyu.edu>
2026-05-28 21:38:28 -0400

    update

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -298,10 +298,11 @@ def get_token_position(processor, text_prompt, image, word, for_comma):
             if ',' in token_str and word in token_str:
                 return index
     else:
-        for index, token_id in enumerate(input_ids):
-            print(processor.tokenizer.decode(token_id).strip().lower())
-            if word in processor.tokenizer.decode(token_id).strip().lower():
-                return index
+        for partitions in range(5):    # dolphin -> 'dol','ph','in'
+            for index, token_id in range(partitions, len(input_ids)):
+                token_id = input_ids[index-partitions:index+1]
+                if word in processor.tokenizer.decode(token_id).strip().lower():
+                    return index
         
     raise ValueError(f"Could not find '{word}' in prompt: {text_prompt}")
 

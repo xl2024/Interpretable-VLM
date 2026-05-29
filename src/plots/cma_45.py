445c4d74afb4c2cdf9c382e5ae9831a76088c55a
xl2024 <xl5333@nyu.edu>
2026-05-29 10:17:11 -0400

    update

--- src/plots/cma_45.py
+++ src/plots/cma_45.py
@@ -264,7 +264,7 @@ def main():
         print("No coco_directory.")
         return
     
-    model_id = "Qwen/Qwen2.5-VL-3B-Instruct"
+    model_id = "Qwen/Qwen2.5-VL-7B-Instruct"
     k_list = [50,100,200]
     cache_dir = "src/data/cma/coco"
     coco_results = run_cma_coco(model_id, k_list, coco_directory, cache_dir)

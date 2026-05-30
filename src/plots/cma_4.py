92c11bae6d32a88e7780677ec3f57c49a26e5089
xl2024 <xl5333@nyu.edu>
2026-05-30 16:33:27 -0400

    update

--- src/plots/cma_4.py
+++ src/plots/cma_4.py
@@ -222,7 +222,7 @@ def main():
         _, num_heads = _resolve_text_model_dims(model)
         _, num_kv_heads = _resolve_text_model_dims(model, kv_heads=True)
         mediation_scores_list = run_mediation_analysis(model_id)
-        mediation_scores = mediation_scores_list[2]
+        mediation_scores = mediation_scores_list[1]
         top_k_heads = get_top_k_heads(mediation_scores, 20)
         top_k_kv_heads = to_kv_heads(top_k_heads, num_heads, num_kv_heads)
         # print("top_k_kv_heads:", top_k_kv_heads)

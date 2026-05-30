ea90eba42efeee901cdb6349adf55da0b52cfe72
xl2024 <xl5333@nyu.edu>
2026-05-30 16:09:49 -0400

    update

--- src/plots/cma_4.py
+++ src/plots/cma_4.py
@@ -179,11 +179,11 @@ def get_patching_results(model, processor, num_layers, num_heads, top_k_heads, l
 
 def main():
     model_id_list = [
-        "Qwen/Qwen2-VL-7B-Instruct",
-        "Qwen/Qwen2.5-VL-3B-Instruct",
-        "Qwen/Qwen2.5-VL-7B-Instruct",
+        # "Qwen/Qwen2-VL-7B-Instruct",
+        "Qwen/Qwen2.5-VL-3B-Instruct"
+        # "Qwen/Qwen2.5-VL-7B-Instruct",
         #  "Qwen/Qwen2.5-VL-32B-Instruct",
-        "llava-hf/llava-1.5-7b-hf"
+        # "llava-hf/llava-1.5-7b-hf"
         #  "llava-hf/llava-1.5-13b-hf"
     ]
 

26684e0e22c341d45ec55d3130590d3fe1506515
xl2024 <xl5333@nyu.edu>
2026-06-01 00:10:19 -0400

    update

--- src/plots/rsa_6.py
+++ src/plots/rsa_6.py
@@ -130,9 +130,9 @@ def rsa_entr_by_model(model_id, save_path):
 
 def main():
     model_id_list = [
-        ("Qwen/Qwen2-VL-7B-Instruct", "6_30"),
-        ("Qwen/Qwen2.5-VL-3B-Instruct", "31"),
-        ("Qwen/Qwen2.5-VL-7B-Instruct", "32"),
+        # ("Qwen/Qwen2-VL-7B-Instruct", "6_30"),
+        # ("Qwen/Qwen2.5-VL-3B-Instruct", "31"),
+        # ("Qwen/Qwen2.5-VL-7B-Instruct", "32"),
         # ("Qwen/Qwen2.5-VL-32B-Instruct", "33"),
         ("llava-hf/llava-1.5-7b-hf", "34"),
         # ("llava-hf/llava-1.5-13b-hf", "35"),

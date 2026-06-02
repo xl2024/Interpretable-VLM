c969ae958bc24f27fb277e328eba5f61e604c2c7
xl2024 <xl5333@nyu.edu>
2026-06-01 22:13:32 -0400

    update

--- src/plots/rsa_6.py
+++ src/plots/rsa_6.py
@@ -84,7 +84,9 @@ def get_trial_data(model, processor, color_list, shape_list):
 
         equiv_shapes = [
             ['airplane', 'plane'],
-            ['x', 'cross']
+            ['x', 'cross'],
+            ['rectangle', 'square']
+            # ['dot', 'sun'] happens but shouldn't be equiv
         ]
         if len(pred) >= 2 and pred[0].strip().lower() == obj_indices[-1]['color'] and is_equiv(pred[1].strip().lower(), obj_indices[-1]['shape'], equiv_shapes):
             corr_trials += 1

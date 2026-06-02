53bddccbd0880e5105dcff140495548a751143b4
xl2024 <xl5333@nyu.edu>
2026-06-02 04:22:11 -0400

    update

--- src/plots/cma_tbl_1.py
+++ src/plots/cma_tbl_1.py
@@ -136,7 +136,7 @@ def main():
         # ("llava-hf/llava-1.5-7b-hf", "LLaVA 1.5 7B"),
         # ("llava-hf/llava-1.5-13b-hf", "LLaVA 1.5 13B")
     ]
-    num_trials = 100
+    num_trials = 10
     top_k = 20
     accs = {}
     for model_id, model_label in model_id_list:

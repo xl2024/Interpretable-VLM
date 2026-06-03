da952ddfd042743b58b0c3c2e2361a9bf50f795f
xl2024 <xl5333@nyu.edu>
2026-06-02 23:03:53 -0400

    update

--- src/plots/cma_46.py
+++ src/plots/cma_46.py
@@ -20,6 +20,7 @@ def get_counting_dataset(num_imgs):
         N = random.randint(3, 8)
         sampled_feat_coords = random.sample(feat_coords, N)
         coords = random.sample(pos_coords, N)
+        print("sampled_feat_coords:", sampled_feat_coords)
         colors = [colors_list[j[0]] for j in sampled_feat_coords]
         shapes = [shapes_list[j[1]] for j in sampled_feat_coords]
         save_path = f"dataset/figure_46/{i}.png"

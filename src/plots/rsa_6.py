7964cbf868debb9e6665ac58d6d77ae1c10b0442
xl2024 <xl5333@nyu.edu>
2026-06-04 18:40:41 -0400

    update

--- src/plots/rsa_6.py
+++ src/plots/rsa_6.py
@@ -256,9 +256,9 @@ def main():
         ("Qwen/Qwen2.5-VL-3B-Instruct", "Qwen 2.5\n3B", "31"),
         ("Qwen/Qwen2.5-VL-7B-Instruct", "Qwen 2.5\n7B", "32"),
         # ("Qwen/Qwen2.5-VL-32B-Instruct", "Qwen 2.5\n32B", "33"),
-        # ("llava-hf/llava-1.5-7b-hf", "LLaVA 1.5\n7B", "34"),
+        ("llava-hf/llava-1.5-7b-hf", "LLaVA 1.5\n7B", "34"),
         # ("llava-hf/llava-1.5-13b-hf", "LLaVA 1.5\n13B", "35"),
-        # ("llava-hf/llava-onevision-qwen2-7b-ov-hf", "LLaVA One\n7B", "36")    # scale up
+        ("llava-hf/llava-onevision-qwen2-7b-ov-hf", "LLaVA One\n7B", "36")    # scale up
     ]
     num_trials = 100
     repeat = 5

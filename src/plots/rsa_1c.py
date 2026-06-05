3b50812e2d0ae60696e46a39b65b60ec7ed4fbe2
xl2024 <xl5333@nyu.edu>
2026-06-05 01:51:02 -0400

    update

--- src/plots/rsa_1c.py
+++ src/plots/rsa_1c.py
@@ -111,12 +111,12 @@ def get_dynamic_token_indices(model: Any, processor: Any, colors: List[str], sha
 
     indices = []
     obj_idx = 0
-    for token_index in range(2, len(input_ids)):    # umbrella=um+bre+lla in LLaVa 1.5 7b
-        token_ids = input_ids[token_index-2:token_index+1]
+    for token_index in range(3, len(input_ids)):    # umbrella=um+bre+lla in LLaVa 1.5 7b
+        token_ids = input_ids[token_index-3:token_index+1]
         token_str = processor.tokenizer.decode(token_ids).strip().lower()
         if token_index >= 588:
             print(token_index, token_str)
-        if ',' in token_str and shapes[shuffle[obj_idx]] in token_str:
+        if ',' == token_str[-1] and shapes[shuffle[obj_idx]] in token_str:
             print(f"obj_idx={obj_idx}, shapes[shuffle[obj_idx]]={shapes[shuffle[obj_idx]]}, token_index={token_index}")
             indices.append({'coords': coords[shuffle[obj_idx]], 'color': colors[shuffle[obj_idx]], 'shape': shapes[shuffle[obj_idx]], 'index': token_index})
             obj_idx += 1

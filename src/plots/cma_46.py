edf24b78e35c87566fa71deb54dbabfdcbb556cb
xl2024 <xl5333@nyu.edu>
2026-06-06 17:13:36 -0400

    update

--- src/plots/cma_46.py
+++ src/plots/cma_46.py
@@ -73,8 +73,8 @@ def cma_counting_by_model(model_id, k_list, dataset):
     mediation_scores = np.zeros((num_layers, num_heads))
     for l,h in [(a, b) for a in range(num_layers) for b in range(num_heads)]:
         mediation_scores[l,h] = max(mediation_scores_list[0][l,h], mediation_scores_list[1][l,h], mediation_scores_list[2][l,h])
-    print("Max top-500 heads:",get_top_k_heads(mediation_scores, k))
-    print("Bottom top-500 heads:",get_top_k_heads(mediation_scores, k, max_k=False))
+    print("Max top-500 heads:",get_top_k_heads(mediation_scores, 500))
+    print("Bottom top-500 heads:",get_top_k_heads(mediation_scores, 500, max_k=False))
     accs = {"Max": {}, "Bottom": {}}
     for cat in accs.keys():
         for k in k_list:

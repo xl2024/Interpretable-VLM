e723fd7ca18c5471902c2c1166c733681529aad6
xl2024 <xl5333@nyu.edu>
2026-06-06 12:08:54 -0400

    update

--- src/plots/cma_46.py
+++ src/plots/cma_46.py
@@ -113,9 +113,9 @@ def main():
     # model_id = "Qwen/Qwen2.5-VL-7B-Instruct"
     model_id = "Qwen/Qwen2.5-VL-32B-Instruct"
     model_name = model_id.replace('/', '_')
-    k_list = [0,10,20,50,100,150,200,250,300,400,500]
-    # k_list = [100]
-    num_imgs = 100
+    # k_list = [0,10,20,50,100,150,200,250,300,400,500]
+    k_list = [0,10,100,200,300,400]
+    num_imgs = 10
     dataset_path = "dataset/figure_46"
     os.makedirs(dataset_path, exist_ok=True)
     dataset = get_counting_dataset(num_imgs, dataset_path)

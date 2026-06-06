709e5fbcf0977c4d98520fdbf85182e93bf3cd79
xl2024 <xl5333@nyu.edu>
2026-06-06 17:12:02 -0400

    update

--- src/plots/cma_46.py
+++ src/plots/cma_46.py
@@ -73,7 +73,8 @@ def cma_counting_by_model(model_id, k_list, dataset):
     mediation_scores = np.zeros((num_layers, num_heads))
     for l,h in [(a, b) for a in range(num_layers) for b in range(num_heads)]:
         mediation_scores[l,h] = max(mediation_scores_list[0][l,h], mediation_scores_list[1][l,h], mediation_scores_list[2][l,h])
-
+    print("Max top-500 heads:",get_top_k_heads(mediation_scores, k))
+    print("Bottom top-500 heads:",get_top_k_heads(mediation_scores, k, max_k=False))
     accs = {"Max": {}, "Bottom": {}}
     for cat in accs.keys():
         for k in k_list:
@@ -115,7 +116,7 @@ def main():
     model_id = "Qwen/Qwen2.5-VL-32B-Instruct"
     model_name = model_id.replace('/', '_')
     # k_list = [0,10,20,50,100,150,200,250,300,400,500]
-    k_list = [0,10,20,50,100,200,300,500]
+    k_list = [10,100,250,500]
     num_imgs = 100
     dataset_path = "dataset/figure_46"
     os.makedirs(dataset_path, exist_ok=True)

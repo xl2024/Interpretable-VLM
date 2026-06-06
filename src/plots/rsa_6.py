027f23aca4b96bfdea536680a508061b6edd75e8
xl2024 <xl5333@nyu.edu>
2026-06-06 04:28:18 -0400

    update

--- src/plots/rsa_6.py
+++ src/plots/rsa_6.py
@@ -98,7 +98,11 @@ def get_trial_data(model, processor, color_list, shape_list, num_trials):
     return trials, corr_trials / num_trials
 
 def rsa_entr_by_model(model_id, num_trials, repeat, save_path):
-    print("=== Starting Figure 6 RSA Reproduction ===")    
+    print("=== Starting Figure 6 RSA Reproduction ===")
+    config = load_config()
+    tier = config['pipeline']['tier']
+    model, processor = load_vlm(model_id, tier)
+    num_layers = get_num_hidden_layers(model)
     colors_list = [
         ['red', 'yellow', 'gray', 'blue', 'pink', 'green', 'black', 'purple', 'orange'],
         ['red', 'green', 'blue', 'red', 'blue', 'red', 'blue', 'green', 'green']
@@ -112,12 +116,6 @@ def rsa_entr_by_model(model_id, num_trials, repeat, save_path):
     for j in range(repeat):
         rsa_results[j] = {}
         for i, entr in enumerate(['High', 'Low']):
-            # try to save vram
-            config = load_config()
-            tier = config['pipeline']['tier']
-            model, processor = load_vlm(model_id, tier)
-            num_layers = get_num_hidden_layers(model)
-
             trials, acc = get_trial_data(model, processor, colors_list[i], shapes_list[i], num_trials)
             
             print(f"\nExecuting RSA across {len(trials)} trials and {num_layers} layers...")
@@ -127,10 +125,10 @@ def rsa_entr_by_model(model_id, num_trials, repeat, save_path):
             rsa_scores_prompt, rsa_scores_last_token = compute_rsa_scores(hidden_states_by_trial, trials, num_layers)
             rsa_results[j][entr] = {"Prompt": rsa_scores_prompt['pos'], "Last": rsa_scores_last_token['pos'], "Acc": acc}
         
-            del model
-            del processor
-            gc.collect()
-            torch.cuda.empty_cache()
+    del model
+    del processor
+    gc.collect()
+    torch.cuda.empty_cache()
 
     plot_rsa_figures(rsa_results=rsa_results[0], num_layers=num_layers, save_path=save_path)
 

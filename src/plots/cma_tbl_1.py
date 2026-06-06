03f09e635bd269ac3821c85f6b396d049fff7379
xl2024 <xl5333@nyu.edu>
2026-06-06 00:43:36 -0400

    update

--- src/plots/cma_tbl_1.py
+++ src/plots/cma_tbl_1.py
@@ -216,17 +216,16 @@ def cma_save_table(accs, save_path):
 
 def main():
     model_id_list = [
-        ("Qwen/Qwen2-VL-7B-Instruct", "Qwen 2-VL 7B"),
+        ("llava-hf/llava-1.5-7b-hf", "LLaVA 1.5 7B"),
+        ("llava-hf/llava-1.5-13b-hf", "LLaVA 1.5 13B"),
         ("Qwen/Qwen2.5-VL-3B-Instruct", "Qwen 2.5-VL 3B"),
         ("Qwen/Qwen2.5-VL-7B-Instruct", "Qwen 2.5-VL 7B"),
-        ("llava-hf/llava-1.5-7b-hf", "LLaVA 1.5 7B"),
-        ("llava-hf/llava-1.5-13b-hf", "LLaVA 1.5 13B")
+        ("Qwen/Qwen2-VL-7B-Instruct", "Qwen 2-VL 7B")
     ]
-    num_trials = 1
-    k_list = [2,5,10,20,50,100]
-    k_list = [200]
+    num_trials = 50
+    # k_list = [2,5,10,20,50,100,200]    # sweeping with num_trials = 1
+    k_list = [50]
     for top_k in k_list:
-        # top_k = 10
         accs = {}
         for model_id, model_label in model_id_list:
             model_name = model_id.replace('/', '_')
